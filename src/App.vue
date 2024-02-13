ea532564f80b8755c2cc663ed080a8f86a8306d2
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2024-02-13 15:55:13 +0100

    ajout header en composant

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router';
-import { ref } from 'vue';
-import router from '@/router/index.js';
-
+import { RouterView } from 'vue-router';
+import Header from "@/components/Header.vue"; // Assurez-vous d'avoir le bon chemin vers le composant Header
 
 </script>
 
 <template>
   <div class="wrapper">
-    <header>
-        <nav class="side">
-          <RouterLink to="/"><img src="./assets/icone.png" style="width: 100px;height: auto;"></RouterLink>
-          <RouterLink to="/Params">Paramètres généraux</RouterLink>
-          <router-link to="/logout"><div class="deco">Déconnexion</div></router-link>
-        </nav>
-    </header>
+    <Header />
 
   <main class="content">
-  <RouterView />
+    <RouterView />
   </main>
 </div>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <nav class="side">
+      <RouterLink to="/"><img src="../assets/icone.png" style="width: 100px;height: auto;"></RouterLink>
+      <RouterLink to="/Params">Paramètres généraux</RouterLink>
+      <router-link to="/logout"><div class="deco">Déconnexion</div></router-link>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
