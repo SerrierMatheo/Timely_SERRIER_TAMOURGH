44d9f39df8934e7a8c84a66e90157c872bcf8c1b
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2024-03-06 19:51:25 +0100

    add timetracker

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,330 @@
-<script setup>
+<template>
+  <div class="activite">
+    <h2>Activité (page d’accueil)</h2>
+
+    <!-- Time Tracker -->
+    <div class="time-tracker">
+      <h3>Time Tracker</h3>
+      <div v-if="!isActivityRunning">
+        <label for="project">Projet :</label>
+        <select id="project" v-model="selectedProject">
+          <option value="" disabled>Sélectionner un projet</option>
+          <option v-for="project in projects" :key="project.id" :value="project.id">{{ project.name }}</option>
+        </select>
+
+        <label for="activity">Type d'activité :</label>
+        <select id="activity" v-model="selectedActivity">
+          <option value="" disabled>Sélectionner un type d'activité</option>
+          <option v-for="activity in activities" :key="activity.id" :value="activity.id">{{ activity.name }}</option>
+        </select>
+
+        <button @click="startEntry">Démarrer l'activité</button>
+      </div>
+
+      <div v-else>
+        <p>Activité en cours : {{ runningActivity.activity.name }}</p>
+        <p>Temps écoulé : {{ formatElapsedTime }}</p>
+        <textarea v-model="entryNotes" placeholder="Notes sur l'activité (Markdown)"></textarea>
+        <button @click="stopEntry">Arrêter l'activité</button>
+      </div>
+    </div>
+
+    <!-- Liste des activités réalisées -->
+    <div>
+      <h3>Activités réalisées ce jour</h3>
+
+      <!-- Filtres facultatifs -->
+      <div class="filters">
+        <label for="projectFilter">Filtrer par projet :</label>
+        <select id="projectFilter" v-model="filters.project">
+          <option value="" disabled>Sélectionner un projet</option>
+          <option v-for="project in projects" :key="project.id" :value="project.id">{{ project.name }}</option>
+        </select>
+
+        <label for="activityFilter">Filtrer par activité :</label>
+        <select id="activityFilter" v-model="filters.activity">
+          <option value="" disabled>Sélectionner un type d'activité</option>
+          <option v-for="activity in activities" :key="activity.id" :value="activity.id">{{ activity.name }}</option>
+        </select>
+
+        <label for="keywordsFilter">Filtrer par mots-clés :</label>
+        <input type="text" id="keywordsFilter" v-model="filters.keywords" />
+      </div>
+
+      <ul>
+        <li v-for="entry in filteredTimeEntries" :key="entry.id">
+          {{ entry.projectName }} - {{ entry.activityName }} - {{ entry.start }} - {{ entry.end }}
+          <button @click="editEntry(entry)">Modifier</button>
+          <button @click="deleteEntry(entry.id)">Supprimer</button>
+        </li>
+      </ul>
+
+      <form v-if="editingEntry" @submit.prevent="updateEntry" class="formulaire">
+        <label for="editEntryProject">Projet :</label>
+        <select id="editEntryProject" v-model="editingEntry.project_id">
+          <option value="" disabled>Sélectionner un projet</option>
+          <option v-for="project in projects" :key="project.id" :value="project.id">{{ project.name }}</option>
+        </select>
+
+        <label for="editEntryActivity">Type d'activité :</label>
+        <select id="editEntryActivity" v-model="editingEntry.activity_id">
+          <option value="" disabled>Sélectionner un type d'activité</option>
+          <option v-for="activity in activities" :key="activity.id" :value="activity.id">{{ activity.name }}</option>
+        </select>
+
+        <label for="editEntryStart">Date et heure de début :</label>
+        <input type="datetime" id="editEntryStart" v-model="editingEntry.start" required />
+
+        <label for="editEntryEnd">Date et heure de fin :</label>
+        <input type="datetime" id="editEntryEnd" v-model="editingEntry.end" required />
+
+        <label for="editEntryComment">Commentaire :</label>
+        <textarea id="editEntryComment" v-model="editingEntry.comment"></textarea>
+
+        <button type="submit" class="btn">Enregistrer les modifications</button>
+      </form>
+
+      <button @click="showManualEntryForm">Ajouter manuellement</button>
+
+      <form v-if="showManualForm" @submit.prevent="createManualEntry">
+        <label for="manualProject">Projet :</label>
+        <select id="manualProject" v-model="manualEntry.project_id">
+          <option value="" disabled>Sélectionner un projet</option>
+          <option v-for="project in projects" :key="project.id" :value="project.id">{{ project.name }}</option>
+        </select>
+
+        <label for="manualActivity">Activité :</label>
+        <select id="manualActivity" v-model="manualEntry.activity_id">
+          <option value="" disabled>Sélectionner un type d'activité</option>
+          <option v-for="activity in activities" :key="activity.id" :value="activity.id">{{ activity.name }}</option>
+        </select>
+
+        <label for="manualStartTime">Date et heure de début :</label>
+        <input type="datetime-local" id="manualStartTime" v-model="manualEntry.start" required>
+
+        <label for="manualEndTime">Date et heure de fin :</label>
+        <input type="datetime-local" id="manualEndTime" v-model="manualEntry.end" required>
+
+        <label for="manualComment">Commentaire :</label>
+        <textarea id="manualComment" v-model="manualEntry.comment" placeholder="Commentaire"></textarea>
+
+        <button type="submit">Ajouter</button>
+      </form>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      projects: [],
+      activities: [],
+      isActivityRunning: false,
+      selectedProject: '',
+      selectedActivity: '',
+      runningActivity: {},
+      entryNotes: '',
+      timeEntries: [],
+      elapsedTime: 0,
+      elapsedTimeInterval: null,
+      filters: {
+        project: '',
+        activity: '',
+        keywords: '',
+      },
+      editingEntry: null,
+      showManualForm: false,
+      manualEntry: {
+        project_id: '',
+        activity_id: '',
+        start: '',
+        end: '',
+        comment: '',
+      },
+    };
+  },
+  computed: {
+    formatElapsedTime() {
+      const hours = String(Math.floor(this.elapsedTime / 3600)).padStart(2, '0');
+      const minutes = String(Math.floor((this.elapsedTime % 3600) / 60)).padStart(2, '0');
+      const seconds = String(this.elapsedTime % 60).padStart(2, '0');
+
+      return `${hours}:${minutes}:${seconds}`;
+    },
+    filteredTimeEntries() {
+      return this.timeEntries.filter((entry) => {
+        const projectMatch = !this.filters.project || entry.project === this.filters.project;
+        const activityMatch = !this.filters.activity || this.filters.activity.includes(entry.activity);
+        const keywordsMatch = !this.filters.keywords || entry.comment.includes(this.filters.keywords);
+
+        return projectMatch && activityMatch && keywordsMatch;
+      });
+    },
+  },
+  methods: {
+    async startEntry() {
+      try {
+        const requestData = {
+          project_id: this.selectedProject,
+          activity_id: this.selectedActivity,
+          comment: this.entryNotes,
+        };
+
+        const response = await this.$api.post('api/time-entries', requestData);
+
+        this.isActivityRunning = true;
+        this.runningActivity = response.data;
+        this.elapsedTime = 0;
+
+        this.elapsedTimeInterval = setInterval(() => {
+          this.elapsedTime++;
+        }, 1000); // Mettez à jour toutes les 1000 ms (1 seconde)
+
+        this.$toast.success('Entrée créée avec succès');
+      } catch (error) {
+        this.$toast.error('Erreur lors de la création de l\'entrée');
+        console.error('Erreur lors de la création de l\'entrée:', error);
+      }
+    },
+    async stopEntry() {
+      try {
+        await this.$api.patch(`api/time-entries/${this.runningActivity.id}/stop`);
+
+        this.isActivityRunning = false;
+        this.runningActivity = null;
+        clearInterval(this.elapsedTimeInterval);
+        this.elapsedTimeInterval = null;
+
+        // Mettez à jour la liste des activités réalisées ce jour
+        await this.fetchTimeEntries();
+
+        this.$toast.success('Entrée stoppée avec succès');
+      } catch (error) {
+        this.$toast.error('Erreur lors de l\'arrêt de l\'entrée');
+        console.error('Erreur lors de l\'arrêt de l\'entrée:', error);
+      }
+    },
+    async editEntry(entry) {
+      this.editingEntry = { ...entry };
+      await this.fetchTimeEntries();
+    },
+    async updateEntry() {
+      try {
+        const response = await this.$api.put(`api/time-entries/${this.editingEntry.id}`, {
+          project_id: this.editingEntry.project_id,
+          activity_id: this.editingEntry.activity_id,
+          start: this.editingEntry.start,
+          end: this.editingEntry.end,
+          comment: this.editingEntry.comment,
+        });
+
+        const updatedEntryIndex = this.timeEntries.findIndex(e => e.id === this.editingEntry.id);
+        if (updatedEntryIndex !== -1) {
+          this.timeEntries[updatedEntryIndex] = response.data;
+        }
+
+        await this.fetchTimeEntries();
+
+        this.$toast.success('Entrée modifiée avec succès');
+        this.editingEntry = null;
+      } catch (error) {
+        this.$toast.error('Erreur lors de la modification de l\'entrée');
+        console.error('Erreur lors de la modification de l\'entrée:', error);
+      }
+    },
+    async deleteEntry(entryId) {
+      try {
+        await this.$api.delete(`api/time-entries/${entryId}`);
+        this.$toast.success('Entrée supprimée avec succès');
+        // Mettre à jour la liste des activités réalisées ce jour
+        await this.fetchTimeEntries();
+      } catch (error) {
+        console.error('Erreur lors de la suppression de l`entrée', error);
+      }
+    },
+    showManualEntryForm() {
+      this.showManualForm = true;
+    },
+
+    async createManualEntry() {
+      try {
+        if (!this.manualEntry.project_id || !this.manualEntry.activity_id || !this.manualEntry.start || !this.manualEntry.end) {
+          this.$toast.error('Veuillez remplir tous les champs obligatoires.');
+          return;
+        }
+
+        await this.$api.post('api/time-entries', this.manualEntry);
+
+        this.manualEntry = {
+          project_id: '',
+          activity_id: '',
+          start: '',
+          end: '',
+          comment: '',
+        };
+        this.showManualForm = false;
+
+        await this.fetchTimeEntries();
+
+        this.$toast.success('Entrée manuelle créée avec succès');
+      } catch (error) {
+        this.$toast.error('Erreur lors de la création de l\'entrée manuelle');
+        console.error('Erreur lors de la création de l\'entrée manuelle:', error);
+      }
+    },
+    async fetchProjectsAndActivities() {
+      try {
+        const projectsResponse = await this.$api.get('api/projects');
+        this.projects = projectsResponse.data;
+
+        const activitiesResponse = await this.$api.get('api/activities');
+        this.activities = activitiesResponse.data;
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données:', error);
+      }
+    },
+    async fetchTimeEntries() {
+      try {
+        const response = await this.$api.get('api/time-entries');
+        const entries = response.data;
+
+        // Effectuez une requête pour récupérer les détails des projets et activités
+        const projectsResponse = await this.$api.get('api/projects');
+        const activitiesResponse = await this.$api.get('api/activities');
+        const projectsMap = projectsResponse.data.reduce((map, project) => {
+          map[project.id] = project.name;
+          return map;
+        }, {});
+        const activitiesMap = activitiesResponse.data.reduce((map, activity) => {
+          map[activity.id] = activity.name;
+          return map;
+        }, {});
+
+        // Ajoutez les noms des projets et des activités aux entrées
+        this.timeEntries = entries.map(entry => ({
+          ...entry,
+          projectName: projectsMap[entry.project_id],
+          activityName: activitiesMap[entry.activity_id],
+        }));
+
+        console.log(this.timeEntries);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des entrées:', error);
+      }
+    },
+  },
+
+  async mounted() {
+    await this.fetchProjectsAndActivities();
+    await this.fetchTimeEntries();
+  },
+};
 </script>
 
-<template>
-  <main>
-    
-    <h2 class="day-text">Welcome on timely</h2>
-  
-  </main>
-</template>
+<style scoped>
+/* Ajoutez du style si nécessaire */
+</style>
