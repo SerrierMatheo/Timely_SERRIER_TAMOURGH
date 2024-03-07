33569f8c4aba571dc695cb8e08d84cc59be5e219
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2024-03-07 16:21:58 +0100

    maj worktime

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
       <RouterLink to="/"><img src="../assets/icone.png" style="width: 100px;height: auto;"></RouterLink>
       <RouterLink to="/Settings">Paramètres généraux</RouterLink>
       <RouterLink to="/Statistiques">Statistiques</RouterLink>
-      <div>{{this.worktime}}</div>
+      <div>Temps totale travaillé : {{ formattedWorkTime }}</div>
       <RouterLink to="/logout"><div class="deco">Déconnexion</div></RouterLink>
     </nav>
   </header>
@@ -12,7 +12,7 @@
 
 <script>
 import { RouterLink } from 'vue-router';
-import {useWorkTimeStore} from "@/stores/workTimeStore.js";
+import { useWorkTimeStore } from "@/stores/workTimeStore.js";
 
 export default {
   data() {
@@ -20,5 +20,22 @@ export default {
       worktime: useWorkTimeStore().workTime,
     };
   },
-}
+  computed: {
+    formattedWorkTime() {
+      return this.formatTime(this.worktime);
+    },
+  },
+  methods: {
+    formatTime(seconds) {
+      const hours = Math.floor(seconds / 3600);
+      const minutes = Math.floor((seconds % 3600) / 60);
+      const remainingSeconds = seconds % 60;
+
+      return `${this.pad(hours)}:${this.pad(minutes)}:${this.pad(remainingSeconds)}`;
+    },
+    pad(value) {
+      return value.toString().padStart(2, '0');
+    },
+  },
+};
 </script>
